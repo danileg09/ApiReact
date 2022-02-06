886a8638e6511371b86c0a362e2bd581dde145e8
daniel <danileg09@gmail.com>
2022-02-06 08:43:16 -0300

    usando state para mudar botão

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
 
+//Arrow function 
   const App = () =>{
-    let message ='hello veinho';
+    //State para atulizar o componete no click
+
+    const[message, setMessage] = useState('hello world'); //state criado junto com a função para chama-lo depois
 
     return( 
       <div>
         <div className= "container">{message}</div>      
-        <button onClick={() => message = 'alouu' }>mudar mensagem</button>
+        <button onClick={() => setMessage('bye bye') }>mudar mensagem</button>  
       </div>  
     )
 };
-
+//state foi chamaddo dentro do listenner onClick com uma função anonima chamando setMessage(função ara chamar o state)
 export default App;
